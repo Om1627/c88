de7e6d4eae2ac697d69c53d65d34bc0c04f0f350
Om1627 <60969676+Om1627@users.noreply.github.com>
2020-12-03 16:02:26 +0530

    2

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -58,7 +58,7 @@ export default class HomeScreen extends Component{
   render(){
     return(
       <View style={{flex:1}}>
-        <MyHeader title="Barter App"/>
+        <MyHeader title="Barter App" navigation={this.props.navigation}/>
         <View style={{flex:1}}>
           {
             this.state.allRequests.length === 0
